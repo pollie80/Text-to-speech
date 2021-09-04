fefb9f3c9e794ccee99e30921870654396ba9813
Tian Welgemoed <tian.welgemoed@gmail.com>
2021-09-04 16:56:41 +1200

    saves what has been said

--- src/TextToSpeechProgram.py
+++ src/TextToSpeechProgram.py
@@ -1,8 +1,17 @@
 import pyttsx3
+from gtts import gTTS #based on https://www.geeksforgeeks.org/convert-text-speech-python/
+import os
 
-engine = pyttsx3.init()
+language = 'en'
+
 
 if __name__ == '__main__':
     what_to_say = "Hello world"
+
+    engine = pyttsx3.init()
     engine.say(what_to_say)
     engine.runAndWait()
+
+    myobj = gTTS(text=what_to_say, lang=language, slow=False)
+    myobj.save("whatissaid.mp3")
+    #os.system("mpg321 welcome.mp3") #tells system to run this if u have run "brew install mpg123"
